99ff17a09ea3ceadff7eac34f3a62a993fc1dea3
msiller455 <msiller455@gmail.com>
2024-07-17 17:41:50 -0700

    finishes tic tac toe game logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,19 @@
+const prompt = require("prompt-sync")()
 
 console.log('Tic Tac Toe')
+
+
+/*------ Constant Lookup Data -----*/
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
 
 /*------ State -----*/
 // These are used to track the 'state' of the game
@@ -10,6 +24,10 @@ let turn, board, winner
 init()
 gameplayLoop()
 
+
+
+
+/*------ Functions -----*/
 // Purpose of init function is to put the state of the game in a "starting position"
 function init() {
   turn = 'X'
@@ -22,11 +40,22 @@ function init() {
   render()
 }
 
+// Purpose of render function is to display our state data to the user in some way shape or form.
 function render() {
-  // Rendering the turn
-  console.log(`It is player ${turn}'s turn`)
+  // Rendering the message
+  renderMessage()
   // Rendering the board
   renderBoard()
+}
+
+function renderMessage() {
+  if (!winner) {
+    console.log(`It is player ${turn}'s turn`)
+  } else if (winner === 'T') {
+    console.log('Tie Game!!!')
+  } else {
+    console.log(`The winner is player ${winner}`)
+  }
 }
 
 function renderBoard() {
@@ -38,13 +67,59 @@ function renderBoard() {
 }
 
 // what goes into a turn of tic tac toe
-// 1. have to choose an empty space
-// 2. put our player's character ('x' or 'o') in that space on the board
-// 3. check if we won the game or not
-// 4. change the turn
+// 1. have player choose an empty space
+//   a. first we need to get the player's input DONE
+//   b. check if the player input is valid (num between 0 and 8) DONE
+//   c. check if the inputed spot on the board is empty DONE
+// 2. put our player's character ('x' or 'o') in that space on the board DONE
+// 3. change the turn
+// 4. check if we won the game or not
+// 5. Keep going until winner or a cats game
 
 function gameplayLoop() {
   while (!winner) {
-    // Contain all the code for what constitutes one turn
+    let playerChoice = prompt('Please enter a number between 0 and 8: ')
+
+    playerChoice = parseInt(playerChoice)
+
+    if (playerChoice >= 0 && playerChoice <= 8) {
+      if (board[playerChoice] === ' ') {
+        // The player has chosen a valid empty spot
+        board[playerChoice] = turn
+        turn = changeTurn()
+        winner = checkWinner()
+        render()
+      } else {
+        console.log('This space is occupied! Please select a different space.')
+      }
+    } else {
+      console.log('This is an invalid number, please choose again.')
+    }
   }
+}
+
+function changeTurn() {
+  if (turn === 'X') {
+    return 'O'
+  } else {
+    return 'X'
+  }
+}
+
+function checkWinner() {
+  for (let combo of WINNING_COMBOS) {
+    if (
+      board[combo[0]] === board[combo[1]]
+      && board[combo[0]] === board[combo[2]]
+      && board[combo[0]] !== ' '
+    ) {
+      return board[combo[0]]
+    }
+  }
+
+  if (!board.includes(' ')) {
+    return 'T'
+  }
+
+  return null
 }
